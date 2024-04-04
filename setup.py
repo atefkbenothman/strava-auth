ee4addb29d2c5842a6491fb3c5cfb25604012153
atefkbenothman <atefkbenothman@gmail.com>
2024-04-04 14:49:11 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="strava_auth",
+  version="0.0.1",
+  description="Streamline the Strava OAuth2 flow with Selenium, allowing developers to quickly and easily integrate Strava's API into their applications",
+  author="Atef Kai Benothman",
+  author_email="atefkbenothman@gmail.com",
+  license="MIT",
+  packages=find_packages(),
+)
