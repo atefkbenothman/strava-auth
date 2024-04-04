e57e43ecf96ab26fd5232fdd5fc627283254d2cd
atefkbenothman <atefkbenothman@gmail.com>
2024-04-04 14:51:20 -0700

    remove email

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
   version="0.0.1",
   description="Streamline the Strava OAuth2 flow with Selenium, allowing developers to quickly and easily integrate Strava's API into their applications",
   author="Atef Kai Benothman",
-  author_email="atefkbenothman@gmail.com",
   license="MIT",
   packages=find_packages(),
 )
