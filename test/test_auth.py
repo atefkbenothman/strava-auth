7c65a6ad23ed6b0de74306ccd6c2c248cb2c4b43
atefkbenothman <atefkbenothman@gmail.com>
2024-04-04 20:31:52 -0700

    update test

--- test/test_auth.py
+++ test/test_auth.py
@@ -2,6 +2,6 @@ from strava_auth.auth import authorize
 
 
 def test_authorize():
-  access_token, athlete = authorize()
+  access_token, athlete = authorize("123", "123", "123", "123", "123")
   assert access_token == "accesstoken"
   assert athlete["athlete"] == "test"
