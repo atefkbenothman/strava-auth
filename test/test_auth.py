4cb7415310fc7c6e5ee38034d97ad314cebcd88b
atefkbenothman <atefkbenothman@gmail.com>
2024-04-04 17:25:35 -0700

    pytest

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,14 +1,7 @@
-import unittest
-
 from strava_auth.auth import authorize
 
 
-class TestAuth(unittest.TestCase):
-  def test_authorize(self):
-    access_token, athlete = authorize()
-    assert access_token == "accesstoken"
-    assert athlete["athlete"] == "test"
-
-
-if __name__ == "__main__":
-  unittest.main()
+def test_authorize():
+  access_token, athlete = authorize()
+  assert access_token == "accesstoken"
+  assert athlete["athlete"] == "test"
